c15425aecac08f500ce349da7c3e0ca0e491733b
rishirajnorthout <rishiraj.pal@northout.com>
2021-10-08 20:03:13 +0530

    React Native Todo App

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+1
+import React from 'react'
+import { Provider as StateProvider } from 'react-redux'
+import store from './store'
+import MainStackNavigator from './AppNavigator'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <StateProvider store={store}>
+      <MainStackNavigator />
+    </StateProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
